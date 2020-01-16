1110440e869ccd006fbd73bbb45e17d673d05c40
iMoeNya <iMoeNya@outlook.com>
2020-01-16 14:04:24 +0800

    Initial Commit

---
+++ RightNow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RightNow
+//
+//  Created by iMoe on 2020/1/16.
+//  Copyright © 2020 imoe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
